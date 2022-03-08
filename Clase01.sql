2c4d444d657b55299bde319a491f70bdf3a6c92e
Carlos Rios <intercrios@hotmail.com>
2022-03-08 11:59:58 -0300

    Add files via upload

---
+++ Clase01.sql
@@ -0,0 +1,50 @@
+/*
+	Curso: Introducción a las bases de datos y SQL		18hs
+    Días: Martes y Viernes 	10:00 a 12:00 hs
+    Profe: Carlos Ríos	carlos.rios@educacionit.com
+    
+    Software:			Paquete XAMPP		apachefriends.org
+						MySQL Workbench		www.mysql.com
+
+	Materiales:			alumni.educacionit.com
+						User: email
+                        Pass: dni
+					
+	Github:				https://github.com/crios2020/MySQLMarzo2022
+    
+    Paquete XAMPP		apachefriends.org
+    
+		X		Windows/Linux/Mac
+        A 		Apache Web Server
+        M		MariaDB
+        P		PHP
+        P		Perl
+        
+	Paquete WAMP	 	wampserver.com
+		W		Windows 
+        A 		Apache Web Server
+        M		MySQL Community Server o MariaDB
+        P 		PHP
+        
+	Paquete LAMP MAMP
+        
+*/
+
+/* Bloque de comentarios */
+-- Comentarios de una sola linea
+
+show databases;				-- muestra las BDs del server
+-- ; es el terminador de sentencias
+-- ctrol - enter 		para ejecutar la linea
+
+SHOW DATABASES;				-- el lenguaje no es case sensitive
+
+create database CursoMySQL;		-- creamos una BD
+
+create database CURSOMYSQL;	
+-- drop database CURSOMYSQL;		-- Borro la BD
+
+use CursoMySQL;					-- activamos la BD
+	
+
+
