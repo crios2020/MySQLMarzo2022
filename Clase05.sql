bf17e2b593045a108b0041275c8ff7a7d86fd052
Carlos Rios <intercrios@hotmail.com>
2022-03-22 11:56:27 -0300

    Add files via upload

---
+++ Clase05.sql
@@ -0,0 +1,10 @@
+-- clase 5
+show databases;
+use negocio;
+show tables;
+describe clientes;
+describe facturas;
+describe articulos;
+select * from clientes;
+select * from facturas;
+select * from artic
